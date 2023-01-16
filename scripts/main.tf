72dcf801f49c6b861198d84bfebad5c25adb9363
dlavric <daniela@hashicorp.com>
2023-01-16 16:52:42 +0100

    modified everything

--- scripts/main.tf
+++ scripts/main.tf
@@ -4,8 +4,8 @@ data "aws_route53_zone" "zone" {
 
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "daniela.${data.aws_route53_zone.zone.name}"
+  name    = "nginxdani.${data.aws_route53_zone.zone.name}"
   type    = "A"
   ttl     = "300"
-  records = [data.aws_instance.public-dns.public_ip]
+  records = "192.168.57.151"
 }
